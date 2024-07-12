6742445e69bab16aab9441638a194c1636147e9b
omkushwaha9 <omkushwaha2005@gmail.com>
2024-07-12 20:25:32 +0530

    BottomSheetMAps

---
+++ components/Maps.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import tw from "twrnc"
+
+
+export default function Maps() {
+  return (
+    <View>
+      <Text>Maps</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
